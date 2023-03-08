237f96ac25ccbcec29d10380207f5365562aed82
MIN-DONGHYUN <mdh2057@naver.com>
2023-03-08 17:30:33 +0900

    2023.03.08 button, innerHTML, document_write, window.alert, console.log, window.onload, var, const, let, equal, typeof

---
+++ march8_javascript/src/main/webapp/_01_where_to/js/myScript.js
@@ -0,0 +1,6 @@
+function myFunction() {			/* 버튼 클릭했을때 실행하는 곳이다. */
+		
+		document.getElementById("demo").innerHTML = "<span style='color: red; font-size: 20pt;'>p 태그의 내용이 변경되었습니다.</span>";      
+		/* document 는 이 문서를 말한다. getElementById(아이디값) 은 p 태그를 잡는다. */
+		/* innerHTML  는 p 태그의 값에 넣는다 */
+	}	
